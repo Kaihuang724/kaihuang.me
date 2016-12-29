ec164f6dda7442709847b0994381db7cee9be8d2
Kai Huang <kaihuang724@gmail.com>
2016-12-28 17:22:07 -0800

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var inject = require('gulp-inject');
+
+gulp.task('styles', function() {
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/css'))
+})
+
+gulp.task('html', ['styles'], function() {
+    var injectFiles = gulp.src(['./dist/css/main.css'])
+
+    gulp.src('./src/index.html')
+        .pipe(inject(injectFiles))
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('default', function() {
+    gulp.watch('./src/sass/**/*.scss', ['styles']);
+    gulp.watch('./src/*.html', ['html']);
+})
